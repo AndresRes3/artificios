8739418d921e64dc4afe6d3a1bfba56ddd92e06b
AndresRes3 <alrestrepog@gmail.com>
2025-03-11 18:18:07 -0500

    feat: se crea funcion contains_artifact_clue

--- app/m_analyzer.py
+++ app/m_analyzer.py
@@ -11,4 +11,38 @@ def contains_artifact_clue (manuscript: list[str]) -> bool:
         
         return True
     
+    #recorrido horizontal
+    for row in manuscript:
+        if character_four(row):
+            return True
+    
+    #Recorrido vertical
+    for col in range(n_cols):
+        column = ''.join(manuscript[row][col] for row in range (n_rows))
+        if character_four(column):
+            return True
+        
+    
+    #recorrido diagonal 
+    for row in range(n_cols):
+        for col in range(n_cols):
+            #hacia abajo
+            if row + 3 < n_rows and col + 3 < n_cols:
+                if manuscript[row][col] == manuscript[row + 1][col +1] == manuscript[row + 2] [col +2] == manuscript[row +3][col +3]:
+                    return True
+            #hacia arriba 
+            if row -3 >=0 and col +3 <n_cols:
+               if manuscript[row][col] == manuscript[row -1][col +1] == manuscript[row-2] [col +2] == manuscript[row -3][col +3]:
+                   return True
+    return False
+                
 
+manuscript = [
+    "RTHGQW",
+    "XRLORE",
+    "NARURR",
+    "REVGAL"
+    "EGSILE",
+    "BRINDS"
+]
+print(contains_artifact_clue(manuscript)) 
